c903bd16218a2e103ed1a69cd82a222ac67aaf80
Pritesh <pritesh7102@gmail.com>
2024-07-10 17:49:41 +0530

    test

--- src/components/flow.jsx
+++ src/components/flow.jsx
@@ -12,33 +12,37 @@ import "reactflow/dist/style.css";
 import HotelPlans from "./hotelPlans";
 import OtaPlans from "./otaPlans";
 
-const initialNodes = [];
-const initialEdges = [];
-
 export default function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [initialNodes, setInitialNodes] = useNodesState([]);
+  const [initialEdges, setInitialEdges] = useEdgesState([]);
   const [yConfirmed, setYConfirmed] = React.useState(50);
   const [yPosition, setYPosition] = React.useState(yConfirmed + 100);
   const [yPositionOta, setYPositionOta] = React.useState(yConfirmed + 100);
-  // const [check, setCheck] = React.useState(false);
 
-  useEffect(() => {
-    edges.forEach((edge) => {
-      const sourceConfirmed = nodes.find((node) => node.id === edge.source);
-      const targetConfirmed = nodes.find((node) => node.id === edge.target);
-      // console.log(sourceConfirmed, targetConfirmed, "confirmed");
-      console.log(sourceConfirmed.position, targetConfirmed.position, "found");
-      handleAddConfirm(
-        sourceConfirmed.data.label,
-        450,
-        sourceConfirmed.id,
-        targetConfirmed.data.label,
-        950,
-        targetConfirmed.id
-      );
-    });
-  }, [edges.length]);
+  // useEffect(() => {
+  //   edges.forEach((edge) => {
+  //     const sourceConfirmed = nodes.find((node) => node.id === edge.source);
+  //     const targetConfirmed = nodes.find((node) => node.id === edge.target);
+  //     console.log(sourceConfirmed.position, targetConfirmed.position, "found");
+  //     console.log(yConfirmed, "yConfirmed position")
+  //     handleAddConfirm(
+  //       sourceConfirmed.data.label,
+  //       450,
+  //       sourceConfirmed.id,
+  //       targetConfirmed.data.label,
+  //       950,
+  //       targetConfirmed.id
+  //     );
+  //   });
+  //   console.log(nodes, "nodes elements");
+  //   console.log(edges, "edges elements")
+  //   const unMappedNodes = nodes.filter((node) => {
+  //     return !edges.some((e) => node.id === e.source || node.id === e.target);
+  //   });
+  //   console.log(unMappedNodes, "unmapped nodes");
+  // }, [edges.length]);
 
   const onConnect = useCallback((params) => {
     console.log(params);
@@ -77,42 +81,44 @@ export default function Flow() {
     setNodes(newNodes);
   };
 
-  const handleAddConfirm = (typeS, positionS, idS, typeT, positionT, idT) => {
-    console.log(typeS, positionS, idS, typeT, positionT, idT, "logging");
-    const newNodeS = {
-      id: idS.toString(),
-      position: { x: positionS, y: yConfirmed },
-      data: { label: typeS },
-      sourcePosition: "right",
-      targetPosition: "left",
-    };
-    const newNodeT = {
-      id: idT.toString(),
-      position: { x: positionT, y: yConfirmed },
-      data: { label: typeT },
-      sourcePosition: "right",
-      targetPosition: "left",
-    };
-    console.log(newNodeS.position, newNodeT.position, "position changed");
-    setYConfirmed(yConfirmed + 100);
-    const temp = nodes.filter((n) => !(n.id === idS || n.id === idT))
-    const newNodes = [...temp, newNodeS, newNodeT];
-    setNodes(newNodes);
-  };
+  // const handleAddConfirm = (typeS, positionS, idS, typeT, positionT, idT) => {
+  //   console.log(typeS, positionS, idS, typeT, positionT, idT, "logging");
+  //   const newNodeS = {
+  //     id: idS.toString(),
+  //     position: { x: positionS, y: yConfirmed },
+  //     data: { label: typeS },
+  //     sourcePosition: "right",
+  //     targetPosition: "left",
+  //   };
+  //   const newNodeT = {
+  //     id: idT.toString(),
+  //     position: { x: positionT, y: yConfirmed },
+  //     data: { label: typeT },
+  //     sourcePosition: "right",
+  //     targetPosition: "left",
+  //   };
+  //   console.log(newNodeS.position, newNodeT.position, "position changed");
+  //   setYConfirmed(yConfirmed + 100);
+  //   const temp = nodes.filter((n) => !(n.id === idS || n.id === idT))
+  //   const newNodes = [...temp, newNodeS, newNodeT];
+  //   setNodes(newNodes);
+  // };
 
   const handleDrag = (e) => {
-    // console.log(e[0]?.position?.x);
-    // if (e[0]?.position?.x < 100 || e[0]?.position?.x > 1000) {
-    //   initialNodes.map((node) => {
-    //     if (node.id === e[0].id) {
-    //       e[0].position.x = node.position.x;
-    //     }
-    //   });
-    // // } else {
-    // //   onNodesChange(e);
-    // // }
     onNodesChange(e);
   };
+
+  const handleConfirm = () => {
+    const mappedNodes = edges.map((edge) => {
+      const source = nodes.find((node) => node.id === edge.source);
+      const target = nodes.find((node) => node.id === edge.target);
+      return {
+        source: source.data.label,
+        target: target.data.label,
+      };
+    });
+    setInitialNodes(mappedNodes);
+  }
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
@@ -129,11 +135,11 @@ export default function Flow() {
         <Panel position="right">
           <OtaPlans handleAdd={handleAddOta} />
         </Panel>
-        {/* <Panel position="bottom-center">
-          <div onClick={handleConfirm} className="button-cms-mapping">
+        <Panel onClick={handleConfirm} position="bottom-center">
+          <div className="button-cms-mapping">
             Confirm Mapping
           </div>
-        </Panel> */}
+        </Panel>
         <Controls />
         <MiniMap />
         <Background variant="dots" gap={12} size={1} />
